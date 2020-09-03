1b040c3984c10f7350e2727884385abd600eaba8
Gérald Pereira <gerald.pereira@octoperf.com>
2020-09-03 23:04:04 +0200

    Unit tests

--- backend/commons/git/git-client-spring/src/test/java/com/octoperf/kraken/git/client/spring/SpringGitClientBuilderTest.java
+++
@@ -1,9 +0,0 @@
-package com.octoperf.kraken.git.client.spring;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-class SpringGitClientBuilderTest {
-
-}
