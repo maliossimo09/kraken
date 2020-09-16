3b44f687f12364f6c4b27ff30ab84dd54d0e561a
Gérald Pereira <gerald.pereira@octoperf.com>
2020-09-16 17:47:33 +0200

    Git implementation using zt-exec

---
+++ backend/commons/git/git-service-cmd/src/main/java/com/octoperf/kraken/git/service/cmd/CmdGitProjectService.java
@@ -0,0 +1,77 @@
+package com.octoperf.kraken.git.service.cmd;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.octoperf.kraken.command.entity.Command;
+import com.octoperf.kraken.command.executor.api.CommandService;
+import com.octoperf.kraken.git.entity.GitConfiguration;
+import com.octoperf.kraken.git.service.api.GitProjectService;
+import com.octoperf.kraken.security.entity.owner.Owner;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileSystemUtils;
+import reactor.core.publisher.Mono;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE, makeFinal = true)
+final class CmdGitProjectService implements GitProjectService {
+
+  private static final Path DOT_GIT_PATH = Paths.get(".git");
+
+  @NonNull OwnerToPath ownerToPath;
+  @NonNull CommandService commandService;
+
+  @Override
+  public Mono<GitConfiguration> connect(final Owner owner, final String repositoryUrl) {
+    final var rootPath = ownerToPath.apply(owner);
+
+    return Mono.fromCallable(() -> Files.createTempDirectory(owner.getUserId()))
+        .flatMap(tmp -> commandService.execute(Command.builder()
+            .path(rootPath.toString())
+            .environment(ImmutableMap.of())
+            .args(ImmutableList.of("git", "clone", repositoryUrl, tmp.toString()))
+            .build())
+            .collectList()
+            .map(strings -> tmp))
+        .flatMap(tmp -> Mono.fromCallable(() -> {
+          // Copy the cloned repo to the application and remove the tmp folder
+          FileSystemUtils.copyRecursively(tmp, rootPath);
+          FileSystemUtils.deleteRecursively(tmp);
+          return null;
+        }))
+        .then(this.getConfiguration(owner));
+  }
+
+  @Override
+  public Mono<GitConfiguration> getConfiguration(final Owner owner) {
+    final var rootPath = ownerToPath.apply(owner);
+    return commandService.execute(Command.builder()
+        .path(rootPath.toString())
+        .environment(ImmutableMap.of())
+        .args(ImmutableList.of("git", "config", "--get", "remote.origin.url"))
+        .build())
+        .next()
+        .map(url -> GitConfiguration.builder().repositoryUrl(url).build());
+  }
+
+  @Override
+  public Mono<Void> disconnect(final Owner owner) {
+    final var rootPath = ownerToPath.apply(owner);
+    return Mono.fromCallable(() -> {
+      FileSystemUtils.deleteRecursively(rootPath.resolve(DOT_GIT_PATH));
+      return null;
+    });
+  }
+}
---
+++ backend/commons/git/git-service-cmd/src/test/java/com/octoperf/kraken/git/service/cmd/CmdGitProjectServiceTest.java
@@ -0,0 +1,82 @@
+package com.octoperf.kraken.git.service.cmd;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.octoperf.kraken.command.entity.Command;
+import com.octoperf.kraken.command.executor.api.CommandService;
+import com.octoperf.kraken.git.entity.GitConfiguration;
+import com.octoperf.kraken.security.entity.owner.OwnerTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+
+import java.nio.file.Paths;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class CmdGitProjectServiceTest {
+
+  @Mock
+  OwnerToPath ownerToPath;
+  @Mock
+  CommandService commandService;
+  @Captor
+  ArgumentCaptor<Command> commandCaptor;
+
+  CmdGitProjectService projectService;
+
+  @BeforeEach
+  public void beforeEach() {
+    projectService = new CmdGitProjectService(ownerToPath, commandService);
+  }
+
+  @Test
+  void shouldConnect() {
+    final var owner = OwnerTest.USER_OWNER;
+    final var repositoryUrl = "repoUrl";
+    final var rootPath = Paths.get("testDir");
+    given(ownerToPath.apply(owner)).willReturn(rootPath);
+    given(commandService.execute(any())).willReturn(Flux.just(repositoryUrl));
+
+    final var config = projectService.connect(owner, repositoryUrl).block();
+    Assertions.assertThat(config)
+        .isNotNull()
+        .isEqualTo(GitConfiguration.builder().repositoryUrl(repositoryUrl).build());
+
+    verify(commandService, times(2)).execute(commandCaptor.capture());
+
+    Assertions.assertThat(commandCaptor.getAllValues()).hasSize(2);
+    final var cloneCmd = commandCaptor.getAllValues().get(1);
+    Assertions.assertThat(cloneCmd.getPath()).isEqualTo(rootPath.toString());
+    Assertions.assertThat(cloneCmd.getArgs().subList(0, cloneCmd.getArgs().size() - 1)).isEqualTo(ImmutableList.of("git", "clone", repositoryUrl));
+
+    final var confCmd = commandCaptor.getAllValues().get(0);
+    Assertions.assertThat(confCmd).isEqualTo(Command.builder()
+        .path(rootPath.toString())
+        .environment(ImmutableMap.of())
+        .args(ImmutableList.of("git", "config", "--get", "remote.origin.url"))
+        .build());
+  }
+
+  @Test
+  void shouldDisconnect() {
+    final var owner = OwnerTest.USER_OWNER;
+    final var root = Paths.get("testDir");
+    final var git = root.resolve(".git").toFile();
+    Assertions.assertThat(git.mkdir()).isTrue();
+    given(ownerToPath.apply(owner)).willReturn(root);
+    projectService.disconnect(owner).block();
+    Assertions.assertThat(git.exists()).isFalse();
+  }
+
+}
